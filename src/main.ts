db43e2bfa96c06ff3eadc1001cc309652816726d
JoeKanaan0 <joekanaan04061961@gmail.com>
2023-12-01 11:32:11 +0200

    Installed dotenv module and added .env to gitignore.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+require('dotenv').config();
+
+let port = process.env.APP_PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port || 3000);
 }
 bootstrap();
