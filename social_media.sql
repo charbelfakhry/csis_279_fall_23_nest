352fadd69c67f82d6a92882c0c51083411ea42f0
aboude <aboudeh003@gmail.com>
2023-12-02 13:28:57 +0200

    Added images table

--- social_media.sql
+++ social_media.sql
@@ -3,16 +3,23 @@ CREATE DATABASE IF NOT EXISTS social_media_db;
 USE social_media_db;
 
 
-CREATE TABLE IF NOT EXISTS users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    full_name VARCHAR(255) NOT NULL,
-    bio TEXT,
-    profile_picture_url VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS images (
+    image_id INT AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL UNIQUE
 );
+
+CREATE TABLE IF NOT EXISTS users (
+                                     user_id INT AUTO_INCREMENT PRIMARY KEY,
+                                     username VARCHAR(255) NOT NULL UNIQUE,
+                                     email VARCHAR(255) NOT NULL UNIQUE,
+                                     password_hash VARCHAR(255) NOT NULL,
+                                     full_name VARCHAR(255) NOT NULL,
+                                     bio TEXT,
+                                     profile_picture INT,
+                                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                     FOREIGN KEY (profile_picture) REFERENCES images(image_id)
+);
+
 
 
 CREATE TABLE IF NOT EXISTS posts (
