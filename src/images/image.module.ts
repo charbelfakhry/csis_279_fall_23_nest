28f601a3132303b208b53708a72e3d4fe3d196d9
aboude <aboudeh003@gmail.com>
2023-12-02 21:40:07 +0200

    refactored images into pictures

--- src/images/image.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ImageService } from './image.service';
-
-@Module({
-  controllers: [],
-  providers: [ImageService],
-})
-export class ImageModule {}
--- src/picture/picture.service.ts
+++ src/picture/picture.service.ts
@@ -1,13 +1,25 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Picture } from './picture.entity';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import path from 'path';
+import process from 'process';
 
 @Injectable()
 export class PictureService {
+  private readonly logger = new Logger(PictureService.name);
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  eraseUnusedFiles() {
+    const PATH_TO_IMAGES = path.join(process.cwd(), 'static', 'images');
+    this.logger.debug(`DELETING UNUSED IMAGES FROM ${PATH_TO_IMAGES}`);
+  }
+
   constructor(
     @Inject('PICTURE_REPOSITORY')
     private pictureRepository: Repository<Picture>,
   ) {}
+
+  generateDefaultProfilePicture() {}
 
   /**
    * This function is an example of how to use the repository in a service.
