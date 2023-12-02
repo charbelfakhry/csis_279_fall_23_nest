565683f810471fcf173ddd10ea63862774b0a7b6
Raseel18 <raseel.bousaid@std.balamand.edu.lb>
2023-12-02 16:19:40 +0200

    The focus of this contribution is on the notifications module, responsible for handling CRUD operations related to user notifications.
    Contributions include CRUD operations for notifications, following a structured approach for DTOs, entities, services, and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
+// app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'your_username',
+      password: 'your_password',
+      database: 'your_database',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    NotificationsModule,
+  ],
 })
 export class AppModule {}
