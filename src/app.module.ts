b11d5be5062c7c8751d7ee4dad33ded7dfeab709
aboude <aboudeh003@gmail.com>
2023-12-02 22:27:57 +0200

    daily filtering of unused images

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,15 @@ import { DatabaseModule } from './database.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { PictureModule } from './picture/picture.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [DatabaseModule, UserModule, PostModule, PictureModule],
+  imports: [
+    DatabaseModule,
+    ScheduleModule.forRoot(),
+    UserModule,
+    PostModule,
+    PictureModule,
+  ],
 })
 export class AppModule {}
--- src/picture/picture.service.ts
+++ src/picture/picture.service.ts
@@ -2,16 +2,34 @@ import { Inject, Injectable, Logger } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Picture } from './picture.entity';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import path from 'path';
-import process from 'process';
+import * as path from 'path';
+import * as fs from 'fs/promises';
+import { cwd } from 'node:process';
 
 @Injectable()
 export class PictureService {
   private readonly logger = new Logger(PictureService.name);
+
+  /**
+   * This function is called once a day at midnight, logs a debug message stating that it's deleting unused images,
+   * then does a set difference between the images in the database and the images saved locally and deletes the images
+   * that only exists locally.
+   */
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
-  eraseUnusedFiles() {
-    const PATH_TO_IMAGES = path.join(process.cwd(), 'static', 'images');
+  async eraseUnusedFiles() {
+    const PATH_TO_IMAGES = path.join(cwd(), 'static', 'images');
     this.logger.debug(`DELETING UNUSED IMAGES FROM ${PATH_TO_IMAGES}`);
+    const allPhotos = new Set(
+      (await this.pictureRepository.find()).map((pic) => pic.picture_url),
+    );
+    const allSavedPhotos = new Set(await fs.readdir(PATH_TO_IMAGES));
+    const toDelete = new Set(
+      [...allSavedPhotos].filter((x) => !allPhotos.has(x)),
+    );
+
+    for (const photo of toDelete) {
+      await fs.rm(path.join(PATH_TO_IMAGES, photo));
+    }
   }
 
   constructor(
