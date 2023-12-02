445c285e4a36b9bc71f6a9e88851220d50792102
JoeKanaan0 <joekanaan04061961@gmail.com>
2023-12-02 17:04:15 +0200

    Removed Middleware from Authentication routes

--- src/authentication/auth.module.ts
+++ src/authentication/auth.module.ts
@@ -1,18 +1,11 @@
-import { Module, MiddlewareConsumer } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtMiddleware } from 'src/middleware/token.middleware';
 
 
 @Module({
-  imports: [JwtMiddleware],
   controllers: [AuthController],
   providers: [AuthService],
 })
 export class AuthModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(JwtMiddleware)
-      .forRoutes(AuthController);
-  }
 }
