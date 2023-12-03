23b3a5a5cce8883652148c7856e11473e3a1faee
Tony Hallal <tonihallal@gmail.com>
2023-12-03 16:18:24 +0200

    added user management

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,3 +10,53 @@ export class UserController {
     return await this.userService.findAll();
   }
 }
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto, UpdateUserDto } from './user.dto';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('/')
+  async getAll() {
+    return await this.userService.findAll();
+  }
+
+  @Get('/users-by-username/:username')
+  async getUsersByUsername(@Param('username') username: string) {
+    return this.userService.findUsersByUsername(username);
+  }
+
+  @Get('/:id')
+  async getUserById(@Param('id') id: string) {
+    return this.userService.findOneById(id);
+  }
+  @Get('/by-username/:username')
+  async getUserByUsername(@Param('username') username: string) {
+    return this.userService.findOneByUsername(username);
+  }
+
+  @Post()
+  async addUser(@Body() user: CreateUserDto) {
+    return this.userService.createUser(user);
+  }
+
+  @Put()
+  async updateUser(@Body() id: string, @Body() user: UpdateUserDto) {
+    return this.userService.updateUser(id, user);
+  }
+
+  @Delete('/:id')
+  async removeUser(@Param('id') id: string) {
+    return this.userService.deleteUser(id);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -16,3 +16,107 @@ export class UserService {
     return this.userRepository.find();
   }
 }
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Like, Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDto, UpdateUserDto } from './user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @Inject('USER_REPOSITORY')
+    private userRepository: Repository<User>,
+  ) {}
+
+  /**
+   * Finds all the users.
+   * @returns Promise<User[]>
+   */
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  /**
+   * Finds all the users by username. Users that match the username or that have the username included
+   * in their username are counted.
+   * @param username
+   * @returns Promise<User[]>
+   */
+  async findUsersByUsername(username: string): Promise<User[]> {
+    const foundUsers = await this.userRepository.find({
+      where: {
+        username: Like(`%${username}%`),
+      },
+    });
+
+    if (!(foundUsers.length > 0)) {
+      throw new NotFoundException(
+        `No user with a username ${username} was found`,
+      );
+    }
+
+    return foundUsers;
+  }
+
+  /**
+   * Finds a user with the corresponding id.
+   * @param user_id
+   * @returns Promise<User | null>
+   */
+  async findOneById(user_id: string): Promise<User | null> {
+    return this.userRepository.findOne({
+      where: { user_id },
+    });
+  }
+
+  /**
+   * Finds a user with the corresponding username
+   * @param username
+   * @returns Promise<User>
+   */
+  async findOneByUsername(username: string): Promise<User> {
+    //search for the user
+    const foundUser = await this.userRepository.findOne({
+      where: { username },
+    });
+
+    //check if the user was found
+    if (!foundUser) {
+      throw new NotFoundException(
+        `User with username: ${username} was not found`,
+      );
+    }
+
+    return foundUser;
+  }
+
+  /**
+   * Creates a new user
+   * @param user
+   * @returns Promise<User>
+   */
+  async createUser(user: CreateUserDto): Promise<User> {
+    const newUser = this.userRepository.create(user);
+    return await this.userRepository.save(newUser);
+  }
+
+  /**
+   * Updates the user with the corresponding id
+   * @param id
+   * @param user
+   * @returns Promise<User | null>
+   */
+  async updateUser(id: string, user: UpdateUserDto): Promise<User | null> {
+    await this.userRepository.update(id, user);
+    return this.findOneById(id); //return the updated user
+  }
+
+  /**
+   * Deletes a user
+   * @param id
+   * @returns Promise<Void>
+   */
+  async deleteUser(id: string) {
+    await this.userRepository.delete(id);
+  }
+}
