eb96c7456996c591a9cb5599e754f40ea9ee4b36
aboude <aboudeh003@gmail.com>
2023-12-03 14:13:46 +0200

    readded notifications

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,13 +1,60 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { NotificationService } from './notification.service';
+import { CreateNotificationDto } from './notifications.dto';
 
 @Controller('notifications')
 export class NotificationController {
-  constructor(private readonly notificationService: NotificationService) {}
+  constructor(private readonly notificationsService: NotificationService) {}
+
+  @Post(':userId')
+  async createNotification(
+    @Param('userId') userId: number,
+    @Body() createNotificationDto: CreateNotificationDto,
+  ) {
+    const notification = await this.notificationsService.createNotification(
+      userId,
+      createNotificationDto,
+    );
+    return { notification };
+  }
 
   @Get('/')
   async getAll() {
-    const notifications = await this.notificationService.findAll();
+    const notifications = await this.notificationsService.findAll();
     return { notifications };
+  }
+
+  @Get(':id')
+  async getNotification(@Param('id') id: number) {
+    const notification =
+      await this.notificationsService.getNotificationById(id);
+    return { notification };
+  }
+
+  @Put(':id')
+  async updateNotification(
+    @Param('id') id: number,
+    @Body() updateNotificationDto: CreateNotificationDto,
+  ) {
+    const updatedNotification =
+      await this.notificationsService.updateNotification(
+        id,
+        updateNotificationDto,
+      );
+    return { updatedNotification };
+  }
+
+  @Delete(':id')
+  async deleteNotification(@Param('id') id: number) {
+    await this.notificationsService.deleteNotification(id);
+    return { message: 'Notification deleted successfully' };
   }
 }
