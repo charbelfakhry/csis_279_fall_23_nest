b112096553f270627588b68b006389820781aaff
JoeKanaan0 <joekanaan04061961@gmail.com>
2023-12-04 18:24:34 +0200

    Enhanced user validation and created custom decorator.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -20,6 +20,7 @@ import e from 'express';
 import { resolve } from 'path';
 import { PictureService } from '../picture/picture.service';
 
+
 @Controller('users')
 export class UserController {
   constructor(
