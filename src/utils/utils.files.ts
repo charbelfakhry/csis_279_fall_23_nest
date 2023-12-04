22a364051d77b6dec8d1028ae3c00e9f63230189
JustLeafy2003 <68219997+JustLeafy2003@users.noreply.github.com>
2023-12-04 16:45:42 +0200

    Create utils.files.ts
    added function for generating a unique file name, based on timestamp and randomized string

---
+++ src/utils/utils.files.ts
@@ -0,0 +1,19 @@
+import * as fs from 'fs';
+
+/**
+ * Generates a unique file name based on timestamp and random string.
+ *
+ * @returns {string} The unique file name.
+ */
+export function generateUniqueFileName(): string {
+  // Get current timestamp and remove non-numeric characters
+  const timestamp = new Date().toISOString().replace(/[^0-9]/g, ''); 
+
+  //Generate a random string
+  const randomString = Math.random().toString(36).substring(7);
+
+  // Combine timestamp and random string to create a unique filename
+  const uniqueFileName = `${timestamp}_${randomString}`;
+
+  return uniqueFileName;
+}
