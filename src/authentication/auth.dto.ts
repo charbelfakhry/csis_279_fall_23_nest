c14a15839e245738acf93821e76d354235835d94
aboude <aboudeh003@gmail.com>
2023-12-05 09:33:23 +0200

    fixed unnecessary steps

--- src/authentication/auth.dto.ts
+++ src/authentication/auth.dto.ts
@@ -5,35 +5,34 @@ import { IsLengthAndNoSpecialChars } from '../decorators/check.length.chars';
  * DTO class for user authentication
  */
 export class AuthUserDto {
-    @IsEmail()
-    email: string;
-  
-    @IsNotEmpty()
-    password: string;
-  
-    constructor(email: string, password: string) {
-      this.email = email;
-      this.password = password;
-    }
-  }
-  
-  /**
-   * DTO class for user registration
-   */
-  export class RegisterUserDto {
+  @IsEmail()
+  email: string;
 
-    @IsEmail()
-    email: string;
-  
-    @IsLengthAndNoSpecialChars(4, 20)
-    username: string;
-  
-    @IsStrongPassword()
-    password: string;
-  
-    constructor(email: string, username: string, password: string) {
-      this.email = email;
-      this.username = username;
-      this.password = password;
-    }
+  @IsNotEmpty()
+  password: string;
+
+  constructor(email: string, password: string) {
+    this.email = email;
+    this.password = password;
   }
+}
+
+/**
+ * DTO class for user registration
+ */
+export class RegisterUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsLengthAndNoSpecialChars(4, 20)
+  username: string;
+
+  @IsStrongPassword()
+  password: string;
+
+  constructor(email: string, username: string, password: string) {
+    this.email = email;
+    this.username = username;
+    this.password = password;
+  }
+}
